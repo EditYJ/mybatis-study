b299f37fa66a091c40d7d25337e231492ce9c5e8
EditYJ <158392613@qq.com>
2020-10-28 14:17:20 +0800

    尝试了下log4j

--- mybatis-02/src/test/java/com/edityj/dao/TestUserMapper.java
+++ mybatis-02/src/test/java/com/edityj/dao/TestUserMapper.java
@@ -3,13 +3,17 @@ package com.edityj.dao;
 import com.edityj.pojo.User;
 import com.edityj.util.MyBatisUtil;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 public class TestUserMapper {
+  private static final Logger logger = Logger.getLogger(TestUserMapper.class);
 
   @Test
   public void getUserById() {
+    logger.debug("进入getUserById方法");
     final SqlSession session = MyBatisUtil.getSession();
+
 
     final UserMapper mapper = session.getMapper(UserMapper.class);
     final User user = mapper.getUserById(2);
